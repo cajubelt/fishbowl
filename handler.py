501cb0008c1aca9ccbb4c903c4b055b378e167e3
Charlie Andrews-Jubelt <charlie@lark.com>
2020-04-19 21:40:51 -0700

    trivial change trying to trigger ci deploy

--- handler.py
+++ handler.py
@@ -18,12 +18,3 @@ def hello(event, context):
 
 def get_word(event, context):
     return {"statusCode": 201, "body": json.dumps({"message": str(randint(1, 50)), "input": event})}
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
