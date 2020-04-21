aada1df150cc92ee39f470cb99d2736f6de6e8a3
Charlie Andrews-Jubelt <charlie@lark.com>
2020-04-20 21:47:23 -0700

    added function: add_word

--- handler.py
+++ handler.py
@@ -1,5 +1,9 @@
 import json
 from random import randint
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+words_table = dynamodb.Table('fishbowl-test-words_table')
 
 
 def hello(event, context):
@@ -14,6 +18,17 @@ def hello(event, context):
     }
 
     return response
+
+
+def add_word(event, context):
+    new_word = json.loads(event['body'])
+
+    words_table.put_item(
+        Item={
+            'word': new_word
+        }
+    )
+    return {'statusCode': 200, 'body': json.dumps({'message': 'added word: ' + new_word, 'input': event})}
 
 
 def get_word(event, context):
