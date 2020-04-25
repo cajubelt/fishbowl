ba9743a9bb3005d78ea6aabb29cc5b9c63e8f4fa
Charlie Andrews-Jubelt <charlie@lark.com>
2020-04-24 23:39:46 -0700

    put all back in bowl endpoint

--- handler.py
+++ handler.py
@@ -27,17 +27,28 @@ def add_word(event, context):
     return {'statusCode': 200, 'body': json.dumps({'message': 'added word: ' + new_word, 'input': event})}
 
 
-def grab_word_from_bowl(event,
-                        context):
-    # should use scan for this to get a bunch of words then choose 1 randomly
-    #   ^ see https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-    words_response = words_table.scan(
+def put_all_back_in_bowl(event, context):
+    out_of_bowl_items = get_all_words(in_bowl=False)['Items']
+    for item in out_of_bowl_items:
+        update_word_status(item['id'], in_bowl=True, active=False)
+    return {'statusCode': 201,
+            "body": json.dumps({"message": str(len(out_of_bowl_items)) + " words put back in bowl."})}
+
+
+def get_all_words(in_bowl: bool) -> dict:
+    return words_table.scan(
         TableName='fishbowl_words',
         FilterExpression="in_bowl = :in",
         ExpressionAttributeValues={
-            ':in': True
+            ':in': in_bowl
         }
     )
+
+
+def grab_word_from_bowl(event, context):
+    # should use scan for this to get a bunch of words then choose 1 randomly
+    #   ^ see https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+    words_response = get_all_words(in_bowl=True)
     response_items = words_response['Items']
     if len(response_items) == 0:
         return {'statusCode': 404,
@@ -46,19 +57,21 @@ def grab_word_from_bowl(event,
     removed_word = random_item['word']
     print('removing ' + removed_word + ' from bowl')
     word_id = random_item['id']
-    set_word_as_out_of_bowl_and_active(word_id)
+    update_word_status(word_id, in_bowl=False, active=True)
     return {"statusCode": 201,
             "body": json.dumps({"word": removed_word,
                                 "word_id": word_id})
             }
 
 
-def set_word_as_out_of_bowl_and_active(word_id: str) -> None:
+def update_word_status(word_id: str,
+                       in_bowl: bool,
+                       active: bool) -> None:
     words_table.update_item(
         Key={'id': word_id},
         UpdateExpression="set in_bowl = :in, active = :active",
         ExpressionAttributeValues={
-            ':in': False,
-            ':active': True
+            ':in': in_bowl,
+            ':active': active
         },
     )
