5b2f4bb216d4c5893cedbd82c958ed7c45299c64
Charlie Andrews-Jubelt <charlie@lark.com>
2020-04-24 22:59:42 -0700

    include id in response, mutate the status (out of bag and active), cleanup

--- handler.py
+++ handler.py
@@ -5,7 +5,8 @@ import uuid
 
 MAX_NUM_WORDS_TO_CONSIDER = 30
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1') #, endpoint_url='https://dynamodb.us-east-1.amazonaws.com')
+dynamodb = boto3.resource('dynamodb',
+                          region_name='us-east-1')
 words_table = dynamodb.Table('fishbowl_words')
 
 
@@ -19,32 +20,45 @@ def add_word(event, context):
         Item={
             'id': str(uuid.uuid1()),
             'word': new_word,
-            # todo add in-bowl status
+            'in_bowl': True,
+            'is_active': False
         }
     )
     return {'statusCode': 200, 'body': json.dumps({'message': 'added word: ' + new_word, 'input': event})}
 
 
-def grab_word_from_bag(event, context):  # todo make empty-body post, include id in response, mutate the status (out of bag and active)
+def grab_word_from_bowl(event,
+                        context):
     # should use scan for this to get a bunch of words then choose 1 randomly
     #   ^ see https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-    try:
-        words_response = words_table.scan(
-            TableName='fishbowl_words',
-            # todo filter for in-bowl words
-        )
-    except:
-        print('bad')
-        return {'statusCode': 1000}
-    print(str(words_response))
+    words_response = words_table.scan(
+        TableName='fishbowl_words',
+        FilterExpression="in_bowl = :in",
+        ExpressionAttributeValues={
+            ':in': True
+        }
+    )
     response_items = words_response['Items']
     if len(response_items) == 0:
         return {'statusCode': 404,
-                'message': 'No words left in bag'}
+                'message': 'No words left in bowl'}
     random_item = random.choice(response_items)
-    print(str(random_item))
-    random_word = random_item['word']
+    removed_word = random_item['word']
+    print('removing ' + removed_word + ' from bowl')
+    word_id = random_item['id']
+    set_word_as_out_of_bowl_and_active(word_id)
     return {"statusCode": 201,
-            "body": json.dumps({"message": random_word,
-                                "input": event})
+            "body": json.dumps({"word": removed_word,
+                                "word_id": word_id})
             }
+
+
+def set_word_as_out_of_bowl_and_active(word_id: str) -> None:
+    words_table.update_item(
+        Key={'id': word_id},
+        UpdateExpression="set in_bowl = :in, active = :active",
+        ExpressionAttributeValues={
+            ':in': False,
+            ':active': True
+        },
+    )
