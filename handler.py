3aeb9b0b73d3557d02f06af5ef0540c69de5694a
Charlie Andrews-Jubelt <charlie@lark.com>
2020-04-24 22:45:07 -0700

    bugfix on tablename, renamed get_word to grab_word_from_bag, added todos

--- handler.py
+++ handler.py
@@ -5,22 +5,8 @@ import uuid
 
 MAX_NUM_WORDS_TO_CONSIDER = 30
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-words_table = dynamodb.Table('fishbowl-test-words_table')
-
-
-def hello(event, context):
-    body = {
-        "message": "Hello Keryn and Chris!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1') #, endpoint_url='https://dynamodb.us-east-1.amazonaws.com')
+words_table = dynamodb.Table('fishbowl_words')
 
 
 def add_word(event, context):
@@ -39,23 +25,25 @@ def add_word(event, context):
     return {'statusCode': 200, 'body': json.dumps({'message': 'added word: ' + new_word, 'input': event})}
 
 
-def get_word(event, context):
+def grab_word_from_bag(event, context):  # todo make empty-body post, include id in response, mutate the status (out of bag and active)
     # should use scan for this to get a bunch of words then choose 1 randomly
     #   ^ see https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-
-    words_response = words_table.scan(
-        ProjectionExpression='word',
-        Limit=MAX_NUM_WORDS_TO_CONSIDER,
-        # todo filter for in-bowl words
-    )
+    try:
+        words_response = words_table.scan(
+            TableName='fishbowl_words',
+            # todo filter for in-bowl words
+        )
+    except:
+        print('bad')
+        return {'statusCode': 1000}
     print(str(words_response))
     response_items = words_response['Items']
     if len(response_items) == 0:
-        return {'statusCode': 500,
-                'error': 'no items in db'}
+        return {'statusCode': 404,
+                'message': 'No words left in bag'}
     random_item = random.choice(response_items)
     print(str(random_item))
-    random_word = random_item['word']['S']
+    random_word = random_item['word']
     return {"statusCode": 201,
             "body": json.dumps({"message": random_word,
                                 "input": event})
